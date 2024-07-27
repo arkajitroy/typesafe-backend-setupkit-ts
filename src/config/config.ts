f5816988e8bffd9f4f33bfc58e805814da7ccc85
arkajitroy <arkajitroy11.official@gmail.com>
2024-07-27 09:04:19 +0530

    feat: modification in the middlewares of the application

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,8 @@ export const LOCAL_IP = '127.0.0.1';
 export const LOCAL_SERVER_PORT = 4000;
 export const LOCAL_MONGO_PORT = 27017;
 
+export const CORS_RESTRICTED_ORIGIN = 'http://localhost:4000';
+
 // DATABASE CONNECTIONS
 export const MONGODB_LOCAL_URI = `mongodb://${LOCAL_IP}:${LOCAL_MONGO_PORT}`;
 export const DATABASE_NAME = 'nodejs-archive';
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import compression from 'compression';
-import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import { LOCAL_SERVER_PORT } from './config/config';
+import cookieParser from 'cookie-parser';
+import { CORS_RESTRICTED_ORIGIN, LOCAL_SERVER_PORT } from './config/config';
 import dbConnect from './config/db.config';
-import startCluster from './config/cluster.config';
 import { Route } from './api/routers';
 
 // Constants
@@ -13,27 +12,38 @@ const app = express();
 dotenv.config();
 
 // middlewares-configuration
-app.use(cors({ credentials: true }));
+app.use(cors({ credentials: true, origin: CORS_RESTRICTED_ORIGIN }));
 app.use(express.json());
 app.use(compression());
-app.use(bodyParser.json({ limit: '50mb', type: 'application/json' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '200kb', type: 'application/json' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(cookieParser());
 
 // Routing Configuration
 app.use('/api/v1', Route);
 
 // Server and Database Connection
-const server = () => {
-  dbConnect()
-    .then(() => {
-      app.listen(LOCAL_SERVER_PORT, () => {
-        console.log('The Backend Server is running @PORT', LOCAL_SERVER_PORT);
-      });
-    })
-    .catch((error: Error) => {
-      console.error('Failed to connect to the database:', error.message);
+dbConnect()
+  .then(() => {
+    app.listen(LOCAL_SERVER_PORT, () => {
+      console.log('The Backend Server is running @PORT', LOCAL_SERVER_PORT);
     });
-};
+  })
+  .catch((error: Error) => {
+    console.error('Failed to connect to the database:', error.message);
+  });
 
+// ============================== (Start the Server using Cluster, only for Big scale Application) ===================
+// const server = () => {
+//   dbConnect()
+//     .then(() => {
+//       app.listen(LOCAL_SERVER_PORT, () => {
+//         console.log('The Backend Server is running @PORT', LOCAL_SERVER_PORT);
+//       });
+//     })
+//     .catch((error: Error) => {
+//       console.error('Failed to connect to the database:', error.message);
+//     });
+// };
 // start server using cluster
-startCluster(server);
+// startCluster(server);
